cea44919a3d63121b4926b9a405911fb52874be2
George Moura <gwmoura@gmail.com>
2017-08-10 08:44:34 -0300

    add blink function

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+import RPi.GPIO as GPIO ## Import GPIO library
+import time ## Import 'time' library. Allows us to use 'sleep'
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,3 +11,14 @@ def index():
 @app.route('/hello')
 def hello():
     return 'Hello, World'
+
+@app.route('/blink/<int:speed>')
+def blink(spped):
+    GPIO.setmode(GPIO.BOARD) ## Use board pin numbering
+    GPIO.setup(7, GPIO.OUT) ## Setup GPIO Pin 7 to OUT
+
+    GPIO.output(7,True)## Switch on pin 7
+    time.sleep(speed)## Wait
+    GPIO.output(7,False)## Switch off pin 7
+    time.sleep(speed)## Wait
+    GPIO.cleanup()
